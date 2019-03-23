e9b6e8da2ec0cac847e3f38c52f293f3753216fc
Jorge Fernández Sánchez <jorge.fernandez@encamina.com>
2019-03-23 19:23:12 +0000

    -- Fix FrameworkVersions

--- bootCampEvents/bootCamp.AzureFunctions/EventGrid.Functions/CreateBlobEvent.cs
+++ bootCampEvents/bootCamp.AzureFunctions/EventGrid.Functions/CreateBlobEvent.cs
@@ -1,13 +1,12 @@
 // This is the default URL for triggering event grid function in the local environment.
 // http://localhost:7071/admin/extensions/EventGridExtensionConfig?functionName={functionname} 
 
+using bootCamp.Shared.Helpers;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
-using bootCamp.Shared.Helpers;
 
 namespace bootCamp.AzureFunctions
 {
@@ -16,7 +15,7 @@ namespace bootCamp.AzureFunctions
         [FunctionName("CreateBlobEvent")]
         public static void Run([EventGridTrigger]JObject eventGridEvent, TraceWriter log)
         {
-            var (action, fileName, isBuilding) = EventGridHelper.GetEventInfo(eventGridEvent);
+            var (action, fileName) = EventGridHelper.GetEventInfo(eventGridEvent);
 
             log.Info(eventGridEvent.ToString(Formatting.Indented));
             
--- bootCampEvents/bootCamp.Shared/Helpers/EventGridHelper.cs
+++ bootCampEvents/bootCamp.Shared/Helpers/EventGridHelper.cs
@@ -4,11 +4,10 @@ namespace bootCamp.Shared.Helpers
 {
     public static class EventGridHelper
     {
-        public static (string action, string fileName, bool isStreet) GetEventInfo(JObject eventGridEvent)
+        public static (string action, string fileName) GetEventInfo(JObject eventGridEvent)
         {
             return (action: (string)eventGridEvent["eventType"],
-                fileName: System.IO.Path.GetFileName((string)eventGridEvent["data"]["url"]),
-                isStreet: ((string)eventGridEvent["data"]["url"]).Contains("edificios"));
+                fileName: System.IO.Path.GetFileName((string)eventGridEvent["data"]["url"]));
         }
     }
 }
