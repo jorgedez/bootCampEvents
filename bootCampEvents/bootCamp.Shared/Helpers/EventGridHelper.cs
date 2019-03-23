590b53843a007266cb4a44e844d56a08446ef1e7
Jorge Fernández Sánchez <jorge.fernandez@encamina.com>
2019-03-23 15:35:51 +0000

    ---

---
+++ bootCampEvents/bootCamp.Shared/Helpers/EventGridHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace bootCamp.Shared.Helpers
+{
+    public static class EventGridHelper
+    {
+        //public static (string action, string fileName, bool isStreet) GetEventInfo(JObject eventGridEvent)
+        //{
+        //    return (action: (string)eventGridEvent["eventType"],
+        //        fileName: System.IO.Path.GetFileName((string)eventGridEvent["data"]["url"]),
+        //        isStreet: ((string)eventGridEvent["data"]["url"]).Contains("edificios"));
+        //}
+    }
+}
