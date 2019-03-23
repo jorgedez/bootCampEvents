352ca75fd6f20e4854bc159e8292b83ec0bdfff6
Jorge Fernández Sánchez <jorge.fernandez@encamina.com>
2019-03-23 23:20:31 +0000

    Added ServiceBus Functions

---
+++ bootCampEvents/bootCamp.AzureFunctions/ServiceBus.Functions/GetPremiere.cs
@@ -0,0 +1,35 @@
+using bootCamp.Shared.Constants;
+using bootCamp.Shared.Entities;
+using bootCamp.Shared.Helpers;
+using Microsoft.ApplicationInsights;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+
+namespace bootCamp.AzureFunctions.ServiceBus.Functions
+{
+    public static class GetPremiere
+    {
+        private static TelemetryClient _telemetry = new TelemetryClient();
+        private const string functionName = "GetPremiere";
+
+        [FunctionName(functionName)]
+        public static void Run([ServiceBusTrigger(QueueNames.Premiere, AccessRights.Manage, Connection = "AzureWebJobsServiceBus")]string sbMessage, TraceWriter log)
+        {
+            try
+            {
+                var hotel = JsonConvert.DeserializeObject<Hotel>(sbMessage);
+                LoggerHelper.WriteTrace(functionName, $"{QueueNames.Premiere.ToUpper()} --> {hotel.name} con precio {hotel.minPrice} |a las {DateTime.UtcNow.ToString("dd/MM/yyyy HH-mm-ss")}", log, TraceLevel.Info, _telemetry);
+
+            }
+            catch (Exception e)
+            {
+                LoggerHelper.TraceException(functionName, log, _telemetry, e);
+                throw e;
+            }
+        }
+    }
+}
